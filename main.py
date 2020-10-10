3f65f71c756e809da04377cacffbcefc06146b86
Fearless <69401479+fearles7810@users.noreply.github.com>
2020-10-10 11:41:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,218 @@
+print("""
+*******************************************
+1. Karakterinizin özelliklerini belirleyin*
+*******************************************
+""")
+adı =  (str(input("1. Savaşçınızın Adı Ne Olsun İstersiniz   ")))
+canı = (int(input("1. Savaşçınızın Canı Kaç olsun İstersiniz")))
+gücü = (int(input("1. Savaşçınız Kaç Vursun İstersiniz      ")))
+
+print("""
+******************************************
+2.Karakterinizin özelliklerini belirleyin*
+******************************************
+""")
+ikinciad = (str(input("2. Savaşçınızın Adı Ne Olsun İstersiniz  : ")))
+ikincican = (int(input("2.savaşçınızın Adı Ne olsun İstersiniz  : ")))
+ad  = (int(input("2. Savaşçınızın Adı Ne Olsun İstersiniz : ")))
+
+
+
+
+adı = {"hp":canı,
+       "güç":gücü }
+
+ikinciad = {"hp":ikincican,
+            "güç":ad}
+
+def vurmak(vuran:dict,vurulan:dict):
+    vurulan["hp"] = vurulan["hp"] - vuran["güç"]
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+input("Vurmak için enter'e basın ")
+vurmak(ikinciad,adı)
+print("1. Savaşçınızın Canı !",adı["hp"])
+
+input("Vurmak için enter'e basın ")
+vurmak(adı,ikinciad)
+print("2. Savaşçınızın canı !",ikinciad["hp"])
+
+
+
+
+
+
+
+
+
+
+
+
